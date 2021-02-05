ad844742abf37c2a417d65a821e82f939d6c9a67
Alexey <enslaver@list.ru>
2021-02-05 03:00:21 +0300

    added RedisHistoryRepository

---
+++ src/main/java/ru/funbox/urlhistoryservice/dao/RedisHistoryRepository.java
@@ -0,0 +1,50 @@
+package ru.funbox.urlhistoryservice.dao;
+
+import io.quarkus.redis.client.RedisClient;
+import io.vertx.redis.client.Response;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Singleton
+public class RedisHistoryRepository implements HistoryRepository {
+    private final RedisClient redisClient;
+    private String groupName;
+
+    @Inject
+    public RedisHistoryRepository(RedisClient redisClient) {
+        this.redisClient = redisClient;
+        this.groupName = "urlHistory";
+    }
+
+    @Override
+    public void saveVisitedDomainsInTime(List<String> domains, long time) {
+        List<String> command = new ArrayList<>();
+
+        command.add(this.groupName);
+
+        domains.forEach(domain -> {
+            command.add(Long.toString(time));
+            command.add(domain);
+        });
+
+        this.redisClient.zadd(command);
+    }
+
+    @Override
+    public List<String> getVisitedDomainsBetweenTime(long from, long to) {
+        List<String> command = new ArrayList<>();
+
+        command.add(this.groupName);
+        command.add(Long.toString(from));
+        command.add(Long.toString(to));
+
+
+        return this.redisClient.zrangebyscore(command).stream()
+                .map(Response::toString)
+                .collect(Collectors.toList());
+    }
+}
